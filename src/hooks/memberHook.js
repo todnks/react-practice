215bdc32749fa0525fb2c500c6c4c654f625c439
todnks <tpdnr01@gmail.com>
2022-08-20 16:36:27 +0900

    로그인관련memberHook으로 따로 관리

---
+++ src/hooks/memberHook.js
@@ -0,0 +1,22 @@
+import { useCallback } from 'react';
+import { useState } from 'react';
+
+function MemberHook(initState) {
+  const [member, setData] = useState(initState);
+  const memberParams = { id: '', password: '', name: '', email: '' };
+  const setMember = useCallback((target) => {
+    for (let paramsKeys in memberParams) {
+      if (paramsKeys === Object.keys(target)[0]) {
+        memberParams[paramsKeys] = Object.values(target)[0];
+      }
+    }
+    setData(memberParams);
+  }, []);
+  return {
+    setMember,
+    setData,
+    member,
+  };
+}
+
+export default MemberHook;
