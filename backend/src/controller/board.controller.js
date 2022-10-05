e42acd253fff9eb671472d8f4757edb10fb561f7
todnks <tpdnr01@gmail.com>
2022-10-05 16:03:55 +0900

    update 조건문추가

--- backend/src/controller/board.controller.js
+++ backend/src/controller/board.controller.js
@@ -6,6 +6,10 @@ export default class BoardController {
     const list = await boardModel.list();
     res.json(list);
   }
+  static async boardView(req, res) {
+    const list = await boardModel.board({ ...req.query });
+    res.json(list);
+  }
 
   static async write(req, res) {
     const errors = validationResult(req);
@@ -23,26 +27,43 @@ export default class BoardController {
 
   static async update(req, res) {
     const errors = validationResult(req);
+    const { title, content, idx } = req.body;
 
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
+
     if (!req.session.user) {
       return res.json({ errors: '로그인후에 접근' });
     }
-    await boardModel.update({ ...req.session.user, ...req.body });
+
+    const { id } = req.session.user;
+    const boardinfo = await boardModel.board({ ...req.body, id: idx });
+
+    if (boardinfo.userId !== id) {
+      return res.status(400).send({ error: '잘못된 접근입니다 ' });
+    }
+
+    await boardModel.update({ ...req.session.user, title, content, idx });
     res.json(true);
   }
-  static async baorddelete(req, res) {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
+  static async boarddelete(req, res) {
     if (!req.session.user) {
       return res.json({ errors: '로그인후에 접근' });
     }
-    await boardModel.delete({ ...req.session.user, ...req.body });
+    const { id } = req.session.user;
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const boardinfo = await boardModel.board(req.query);
+    if (req.session.userid !== boardinfo.userid) {
+      return res.status(400).send({ error: '잘못된 접근입니다 ' });
+    }
+
+    await boardModel.delete({ userId: id, id: req.query.id });
     res.json(true);
   }
 }
